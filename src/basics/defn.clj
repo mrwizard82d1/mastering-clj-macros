fa3664931445538a966197302dae2a10b4661104
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-01-23 14:35:21 -0600

    Add simple definition.

---
+++ src/basics/defn.clj
@@ -0,0 +1,9 @@
+(ns basics.defn)
+
+(defn my-mapcat
+  "Returns the result of applying concat to the result of applying map to f and colls.
+  Thus function f should return a collection."
+  {:added "1.0"
+   :static true}
+  [f & colls]
+  (apply concat (apply map f colls)))
